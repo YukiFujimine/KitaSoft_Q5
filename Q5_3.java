29d9d3aa75e39573d54137fc3d8f53aaa7e9ccbc
YukiFujimine <study.yuki0115@gmail.com>
2020-01-27 12:33:34 +0900

    北ソフトQ5_3

--- Q5_3.java
+++ Q5_3.java
@@ -1,8 +1,10 @@
+//10 個の数値を入力する。
+//入力された数値を偶数と奇数に分類して表示する。
 import java.util.*;
 public class Q5_3{
 		public static void main(String[] args){
 				int[] log=new int[10];
-				for(int i=0;i<10;i++){
+				for(int i=0;i<log.length;i++){
 						System.out.print("数値を入力してください > ");
 						int input=new Scanner(System.in).nextInt();
 						log[i]=input;
